73687861b9210c6db75d83e34f4eddabe9c41eb1
Matthias Leuffen <matthes@leuffen.de>
2023-11-09 23:07:45 +0100

    update

---
+++ src/Driver/PhpmailerDriver.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Lack\MailSpool\Driver;
+
+class PhpmailerDriver
+{
+
+}
--- src/MailSpooler.php
+++ src/MailSpooler.php
@@ -2,9 +2,17 @@
 
 namespace Lack\MailSpool;
 
+use Brix\Core\Type\BrixEnv;
+use Phore\FileSystem\PhoreDirectory;
+
 class MailSpooler
 {
 
+
+    public function __construct(public string|PhoreDirectory $spoolDir)
+    {
+        $this->spoolDir = phore_dir($spoolDir);
+    }
 
 
     public function spoolMail(OutgoingMail $mail)
